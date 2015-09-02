ccffb6671be2d76e970b3f1b60cbdd7880b728fb
631068264 <631068264@qq.com>
2015-09-02 22:39:04 +0800

    基本变量

---
+++ hello.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+# __author__ = 'wuyuxi'
+
+print("Hello world")
+
+print "flalflajf aljfla;f 46464646"
+
+print "464", "4546f4645f46a"  # 字符串拼接
+
+print '200 + 100 = ', 100 + 200
+
+name = raw_input('输入你的名字 ')  # 控制台输入
+print 'hello', name
